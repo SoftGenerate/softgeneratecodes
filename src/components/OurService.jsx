b7bd88ff8dd06a26f874b872e8edece5d39aedc5
vyasnikhil123 <vyasnikhil123>
2020-07-11 14:06:57 +0530

    Theme change and code updated from template

--- src/components/OurService.jsx
+++ src/components/OurService.jsx
@@ -3,8 +3,33 @@ import React, { Component } from 'react'
 class OurServices extends Component {
     render() {
         return (
-            <div style={{ background: 'gray' }}>OurServices</div>
-        )
+          // <div style={{ background: 'gray' }}>OurServices</div>
+          <div id="services" className="text-center">
+            <div className="container">
+              <div className="section-title">
+                <h2>Our Services</h2>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit duis
+                  sed dapibus leonec.
+                </p>
+              </div>
+              <div className="row">
+                {this.props.data
+                  ? this.props.data.map((d) => (
+                      <div className="col-md-4">
+                        {" "}
+                        <i className={d.icon}></i>
+                        <div className="service-desc">
+                          <h3>{d.name}</h3>
+                          <p>{d.text}</p>
+                        </div>
+                      </div>
+                    ))
+                  : "loading"}
+              </div>
+            </div>
+          </div>
+        );
     }
 }
 
--- src/components/Testimonial.jsx
+++ src/components/Testimonial.jsx
@@ -3,8 +3,33 @@ import React, { Component } from 'react'
 class Testimonial extends Component {
     render() {
         return (
-            <div style={{ background: 'gray' }}>Testimonial</div>
-        )
+          // <div style={{ background: 'gray' }}>Testimonial</div>
+          <div id="testimonials">
+            <div className="container">
+              <div className="section-title text-center">
+                <h2>What our clients say</h2>
+              </div>
+              <div className="row">
+                {this.props.data
+                  ? this.props.data.map((d) => (
+                      <div className="col-md-4">
+                        <div className="testimonial">
+                          <div className="testimonial-image">
+                            {" "}
+                            <img src={d.img} alt="" />{" "}
+                          </div>
+                          <div className="testimonial-content">
+                            <p>"{d.text}"</p>
+                            <div className="testimonial-meta"> - {d.name} </div>
+                          </div>
+                        </div>
+                      </div>
+                    ))
+                  : "loading"}
+              </div>
+            </div>
+          </div>
+        );
     }
 }
 
